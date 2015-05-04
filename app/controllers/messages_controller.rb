6a8bbc9a257cce31a4a514734dd6a038b43ec07e
Charles <charles@RMED150LT.(none)>
2015-05-04 18:39:37 +0530

    change in msg controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,17 +7,17 @@ class MessagesController < ApplicationController
 
 	def create
 		@receiver_id = params[:recepient_id].to_i
+		if params[:message].present?
+	  	if params[:message]["content"].present?
+	  	  Message.create(user_id: current_user.id, sender_id: current_user.id, receiver_id: @receiver_id, content: params[:message]["content"] )
+	    end
+    end
 		message1 = Message.where("sender_id IN (?) AND receiver_id IN (?)", current_user.id ,@receiver_id)
 		message2 = Message.where("sender_id IN (?) AND receiver_id IN (?)", @receiver_id, current_user.id)
 		messages = message1 + message2
 		if messages.present?
 			@messages = messages.sort_by{|e| e[:id]}
 		end
-	  if params[:message].present?
-	  	if params[:message]["content"].present?
-	  	  Message.create(user_id: current_user.id, sender_id: current_user.id, receiver_id: @receiver_id, content: params[:message]["content"] )
-	    end
-    end
 	  @message = Message.new 
 	end
 		
